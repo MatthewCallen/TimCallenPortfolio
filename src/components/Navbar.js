e03dff197568416c85e907db3a88b62434a3e0d8
Matthew <mattdcallen2004@gmail.com>
2024-06-06 16:09:55 -0400

    committing logo:

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../assets/logo.png';
+
+function navbar() {
+  return (
+    <div className='Navbar'>
+        <div className = 'leftSide'>
+            <img src={Logo} alt="Logo"/>
+        </div>
+
+        <div className='rightSide'>
+            
+        </div>        
+
+      
+    </div>
+  )
+}
+
+export default navbar
